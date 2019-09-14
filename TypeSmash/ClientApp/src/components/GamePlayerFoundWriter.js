6ac9241cbce1fb814d8b8d9532316607ab167af4
xred13 <fabiosfendrychramos10@gmail.com>
2019-09-14 18:10:55 +0200

    Removed console.logs used for fixing an issue.

--- TypeSmash/ClientApp/src/components/GamePlayerFoundWriter.js
+++ TypeSmash/ClientApp/src/components/GamePlayerFoundWriter.js
@@ -9,31 +9,23 @@ export default class GamePlayerRoleWriter extends Component{
 
     hasCharactersAfterSpace = (word) => {
         // if there is a space in the word and it isn't the last character, returns true
-        console.log("does it have space?: " + word);
         for(let i = 0; i < word.length-1; i++){
             if(word[i] === " "){
-                console.log("has space");
                 return true;
             }
         }
 
-        console.log("doesn't have space");
         return false;
     }
 
     separateInputWords = (word) => {
         let spacePosition = word.search(" ");
-        console.log("Space position: " + spacePosition);
-        console.log("word: " + word);
         let firstWord = word.slice(0, spacePosition+1), secondWord = word.slice(spacePosition+1, word.length);
-        console.log(firstWord + ";" + secondWord);
 
         return [firstWord, secondWord]
     }
 
     keyPressed = (e) => {
-
-        console.log("key pressed: " + String.fromCharCode(e.keyCode));
 
         let charEnteredKeyCode = e.keyCode;
 
@@ -49,7 +41,6 @@ export default class GamePlayerRoleWriter extends Component{
           // a fix to the fact we can write fast and add letters to the input field while keyPressed is being called
           if(this.hasCharactersAfterSpace(inputText)){
             [inputText, e.target.value] = this.separateInputWords(inputText);
-            console.log("split words: " + inputText + ";" + e.target.value);
           }
           else{
             e.target.value = "";
@@ -58,7 +49,6 @@ export default class GamePlayerRoleWriter extends Component{
           let charEntered = String.fromCharCode(charEnteredKeyCode.toString());
 
           sendNewWrittenWord(inputText);
-          console.log("setting state to: " + inputText + ";");
           this.setState({ writtenText: this.state.writtenText + inputText });
         }
     }
